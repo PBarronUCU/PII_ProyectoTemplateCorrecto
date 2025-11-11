c3bca1f7bce1afae878c9f32723f39e404108397
PBarronUCU <PATRICIO.BARRON@correo.ucu.edu.uy>
2025-11-10 22:42:38 -0300

    Cosas del Doxygen

---
+++ docs/html/search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "acdehilpqrst",
+  1: "r",
+  2: "acdehilpqrst"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "files",
+  2: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Files",
+  2: "Pages"
+};
+
